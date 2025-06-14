1952e5f1134f3a3e58281710af64cd1155da3dcc
Douglas Pereira <douglasppa@gmail.com>
2025-06-14 00:23:25 -0300

    feat: adicionar monitoramento com Prometheus e dashboard base no Grafana

---
+++ app/core/config.py
@@ -0,0 +1,29 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    # Application settings
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "TaskFlow"
+
+    # PostgreSQL connection
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+
+    # Authentication settings
+    SECRET_KEY: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    # RabbitMQ connection
+    RABBITMQ_USER: str
+    RABBITMQ_PASS: str
+
+    # MongoDB connection
+    MONGO_URL: str
+    MONGO_DB_NAME: str
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+from app.core.config import settings
+
 from app.api.v1.routes import router as api_router
 from app.api.v1.routes_task import router as task_router
 from app.api.v1.routes_auth import router as auth_router
-from prometheus_fastapi_instrumentator import Instrumentator
 
-app = FastAPI(title="TaskFlow")
+app = FastAPI(title=settings.PROJECT_NAME)
 
 Instrumentator().instrument(app).expose(app)
 
-app.include_router(api_router, prefix="/api/v1")
-app.include_router(task_router, prefix="/api/v1")
-app.include_router(auth_router, prefix="/api/v1")
+app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(task_router, prefix=settings.API_V1_STR)
+app.include_router(auth_router, prefix=settings.API_V1_STR)
