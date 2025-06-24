2dbb82be21a91a09efb6827b12e82a8c3237ecac
Douglas Pereira <douglasppa@gmail.com>
2025-06-24 14:39:39 -0300

    fix: adjust RabbitMQ URL for Render

--- app/workers/celery_app.py
+++ app/workers/celery_app.py
@@ -1,12 +1,7 @@
 from celery import Celery
 import os
 
-RABBITMQ_USER = os.getenv("RABBITMQ_USER", "guest")
-RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", "guest")
-RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
-RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", "5672")
-
-CELERY_BROKER_URL = f'amqp://{RABBITMQ_USER}:{RABBITMQ_PASSWORD}@{RABBITMQ_HOST}:{RABBITMQ_PORT}//'
+CELERY_BROKER_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@localhost:5672//")
 
 celery_app = Celery(
     "worker",
