8ba2ea49b5209056e6543b25170e45c71ca82c6d
Douglas Pereira <douglasppa@gmail.com>
2025-06-24 20:56:08 -0300

    fix: adjust tests and database params

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -2,7 +2,11 @@
 import pytest
 from http import HTTPStatus
 
-def test_healthcheck(client):
-    response = client.get("/api/v1/health")
+def test_liveness(client):
+    response = client.get("/api/v1/health/live")
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {"status": "ok"}
+    assert response.json() == {"status": "alive"}
+
+def test_readiness(client):
+    response = client.get("/api/v1/health/ready")
+    assert response.status_code == HTTPStatus.OK
