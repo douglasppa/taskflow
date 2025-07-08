8eb4b37639e185ffb7bd549bfb9cfa8060a47f37
Douglas Pereira <douglasppa@gmail.com>
2025-07-08 16:56:20 -0300

    fix: Ajustes para ambiente do Render reconhecer o serviço

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,6 +35,7 @@ app = FastAPI(title=settings.PROJECT_NAME, lifespan=lifespan)
 origins = [
     "http://localhost:5173",  # Vite local
     "http://127.0.0.1:5173",  # Alternativo
+    "https://taskflow-frontend-ylgl.onrender.com",  # Frontend em produção
 ]
 
 app.add_middleware(
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -11,7 +11,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axios.post(
-        'http://localhost:8000/api/v1/auth/login',
+        `${import.meta.env.VITE_API_URL}/api/v1/auth/login`,
         {
           email,
           password,
